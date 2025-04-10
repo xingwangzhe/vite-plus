ab61129282a6180bd24eeae1558ceb6e28ef9f63
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-10 09:20:19 +0800

    refactor: replace dependencies

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,26 +1,30 @@
-import { cac } from 'cac';
-import shell from 'shelljs';
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+import { x } from 'tinyexec'
 
-const cli = cac('vp');
+const args = hideBin(process.argv)
+const commandArgs = args.slice(1)
 
-cli.command('dev', '');
-cli.command('build', '');
-cli.command('preview', '');
-cli.command('lib', '');
-cli.command('run', '');
-cli.command('lint', '')
-  .action(() => {
-    shell.exec('./node_modules/.bin/oxlint');
-  });
-cli.command('fmt', '');
-cli.command('test', '');
-cli.command('bench', '');
-cli.command('docs', '');
-cli.command('publish', '');
-cli.command('ui', '');
+const cli = yargs(args).scriptName('vite-plus')
 
-cli.help();
+cli.command(['$0', 'dev'], '', ({ argv }) => {
+  console.log('dev command', argv)
+})
+cli.command('build', '')
+cli.command('preview', '')
+cli.command('lib', '')
+cli.command('run', '')
+cli.command('lint', '', () => {
+  return x('./node_modules/.bin/oxlint', commandArgs, {
+    nodeOptions: { stdio: 'inherit' },
+  })
+})
 
-cli.version('0.0.0');
+cli.command('fmt', '')
+cli.command('test', '')
+cli.command('bench', '')
+cli.command('docs', '')
+cli.command('publish', '')
+cli.command('ui', '')
 
-cli.parse();
+cli.help().parse()
